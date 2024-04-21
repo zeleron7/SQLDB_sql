b4093ae1908038fb52662af0a5fc26a8c8638bc2
Martin <martin_lenart@icloud.com>
2024-04-21 13:57:34 +0200

    update

--- docker/create_sqlserver2022_docker.sh
+++ docker/create_sqlserver2022_docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#ensure rosetta2 is installed on your M1 or M2 machine to emulate x86
+
+#Only on macOS ensure rosetta2 is installed on your M1 or M2 machine to emulate x86
 softwareupdate --install-rosetta
+
+
+#Only on Windows11 ensure you have last version of WindowsLinuxSubsystem installed
+wsl --update
 
 #pull the container image to my computer
 docker pull mcr.microsoft.com/mssql/server:2022-latest
 
 #install and run the container 
-#docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=skYhgS@83#aQ' -p 1433:1433 --name sqledgecontainer -d mcr.microsoft.com/azure-sql-edge
-
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=skYhgS@83#aQ" -p 1433:1433 --name sql2022container --hostname sql2022 -d mcr.microsoft.com/mssql/server:2022-latest
+#Note that I change the SQL Server port from 1433 to 14333 by using -p 14333:143  - this is to avoid conflict with 
+#already installed sql servers
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=skYhgS@83#aQ" -p 14333:1433 --name sql2022container1 --hostname sql2022 -d mcr.microsoft.com/mssql/server:2022-latest
